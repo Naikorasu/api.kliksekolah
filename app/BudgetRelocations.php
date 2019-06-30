2720e43bf9de9afb2bd735a4db62aa0a0b916b66
Martin Yohansa <martin.yohansa@gmail.com>
2019-06-30 15:58:24 +0700

    Added total in budget relocation

--- app/BudgetRelocations.php
+++ app/BudgetRelocations.php
@@ -2,7 +2,10 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
 use App\Budgets;
 
 class BudgetRelocations extends Model
