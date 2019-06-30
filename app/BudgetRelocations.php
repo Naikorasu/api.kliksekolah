1ab8397ffee42c414e260cd3e32308cfa1a58e23
Martin Yohansa <martin.yohansa@gmail.com>
2019-06-30 15:54:40 +0700

    Added total in budget relocation

--- app/BudgetRelocations.php
+++ app/BudgetRelocations.php
@@ -9,12 +9,20 @@ class BudgetRelocations extends Model
 {
     protected $fillable = [
       'user_id',
+      'nomor_pengajuan',
       'approved',
       'submitted',
       'head',
       'account',
       'description'
     ];
+
+    public function scopeTotalPengajuan() {
+      return $query
+            ->select(['*',
+              DB::raw('total - (select IFNULL(SUM(relocated_amount),0) from budget_relocation_sources where budget_relocation_sources.budget_relocation_id = budget_relocations.id group by budget_relocation_id) as total')
+            ]);
+    }
 
     public function budgetRelocationSources() {
       return $this->hasMany(BudgetRelocationSources::class , 'budget_relocation_id', 'id');
--- app/Services/BudgetDetailRelocationsService.php
+++ app/Services/BudgetDetailRelocationsService.php
@@ -28,7 +28,7 @@ class BudgetDetailRelocationsService extends BaseService {
 
   public function list($filters=[]) {
     $conditions = $this->buildFilters($filters);
-    return BudgetRelocations::where($conditions)->get();
+    return BudgetRelocations::totalPengajuan()->where($conditions)->get();
   }
 
   public function get($id) {
