c12df7c7d3c2a24c6274ebb9506559651d5fed62
Martin Yohansa <martin.yohansa@gmail.com>
2019-06-30 16:21:34 +0700

    nomor pengajuan

---
+++ database/migrations/2019_06_30_162052_add_nomor_pengajuan.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNomorPengajuan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('budget_relocations', function (Blueprint $table) {
+          $table->string('nomor_pengajuan',255)->nullable();
+      })
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('budget_relocations', function (Blueprint $table) {
+            //
+        });
+    }
+}
