604229e6c693f7fc54c2b4fe2046ae344bd9795e
Martin Yohansa <martin.yohansa@gmail.com>
2019-07-29 13:52:31 +0700

    Fixing bug on get journal

--- app/Services/JournalsService.php
+++ app/Services/JournalsService.php
@@ -88,7 +88,7 @@ class JournalsService extends BaseService {
     try {
       if($type == 'KAS' || $type == 'BANK') {
         $data = $journal->with('journalDetails.journalCashBankDetails')->findOrFail($id)->toJson();
-        $data['details'] = [
+        $data->details = [
           'standard' => [],
           'reconciliation' => []
         ];
