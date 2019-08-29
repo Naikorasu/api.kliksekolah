76cd3a0bbd0f1961935fc5b1b02b5696d10b0a2b
Martin Yohansa <martin.yohansa@gmail.com>
2019-08-30 00:18:05 +0700

    Mapped fundRequestDetails

--- app/Services/FundRequestsService.php
+++ app/Services/FundRequestsService.php
@@ -63,7 +63,15 @@ class FundRequestsService extends BaseService {
 
   public function get($id) {
     $fundRequest = FundRequests::with('fundRequestDetails','fundRequestDetails.budgetDetail', 'fundRequestDetails.budgetDetail.parameter_code', 'head')->find($id);
+    $fundRequest->fundRequestDetails->map(function($item) {
+      return {
+        'amount' => $item['amount'],
+        'parameter_code' => $item['budget_detail']['parameter_code'],
+        'budget_detail_id' => $item['budget_detail']['id']
+      }
+    });
     $fundRequest->details = $fundRequest->fundRequestDetails;
+
     return $fundRequest;
   }
 
