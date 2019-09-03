82d1f370c0a600161f922e8ded0ee11cd4a19b36
Martin Yohansa <martin.yohansa@gmail.com>
2019-09-03 09:58:53 +0700

    Fixing bug

--- app/Services/BudgetDetailRelocationsService.php
+++ app/Services/BudgetDetailRelocationsService.php
@@ -47,7 +47,7 @@ class BudgetDetailRelocationsService extends BaseService {
   public function get($id) {
     try {
       $budgetRelocation = BudgetRelocations::with('head', 'budgetRelocationSources', 'budgetRelocationRecipients', 'budgetRelocationSources.budgetDetail', 'budgetRelocationRecipients.budgetDetailDraft', 'budgetRelocationSources.budgetDetail.parameter_code', 'budgetRelocationRecipients.budgetDetailDraft.parameter_code')->findOrFail($id);
-      $budgetRelocation->head->title = $budgetRelocation->head->desc;
+      $budgetRelocation->head['title'] = $budgetRelocation->head['desc'];
       return $budgetRelocation;
     } catch (ModelNotFoundException $exception) {
       throw new DataNotFoundException($exception->getMessage());
