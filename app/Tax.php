9cace8445cf7870d44c240c86a6cf421ab3e7798
Martin Yohansa <martin.yohansa@gmail.com>
2019-09-12 22:55:29 +0700

    Fixing Tax-TaxFields relationships

--- app/Tax.php
+++ app/Tax.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tax extends Model
 {
     protected $table = 'tax';
-    
+
     protected $fillable = [
       'journal_cash_bank_details_id',
       'type',
@@ -20,6 +20,6 @@ class Tax extends Model
     }
 
     public function taxFields() {
-      return $this->hasMany('App\taxFields');
+      return $this->hasMany('App\TaxFields');
     }
 }
