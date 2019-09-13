5523fac488743cae6b3b48ee52dc616a8e9df3be
Martin Yohansa <martin.yohansa@gmail.com>
2019-09-13 23:35:52 +0700

    Adding priority to userGroups

--- database/seeds/UserGroupsTableSeeder.php
+++ database/seeds/UserGroupsTableSeeder.php
@@ -15,24 +15,31 @@ class UserGroupsTableSeeder extends Seeder
         UserGroups::insert([
           'name' => 'Super Admin',
           'description' => 'Super Admin',
+          'priority' => 0
         ], [
           'name' => 'Admin',
           'description' => 'Admin',
+          'priority' => 1
         ], [
           'name' => 'Keuangan Unit',
           'description' => 'Keuangan Unit',
+          'priority' => 6
         ], [
           'name' => 'Kepala Sekolah',
-          'description' => 'Kepala Sekolah'
+          'description' => 'Kepala Sekolah',
+          'priority' => 5
         ], [
           'name' => 'Ketua Perwakilan',
           'description' => 'Ketua Perwakilan',
+          'priority' => 4
         ], [
           'name' => 'Keuangan Pusat',
-          'description' => 'Keuangan Pusat'
+          'description' => 'Keuangan Pusat',
+          'priority' => 3
         ], [
           'name' => 'Bendahara',
-          'description' => 'Bendahara'
+          'description' => 'Bendahara',
+          'priority' => 2
         ]);
     }
 }
